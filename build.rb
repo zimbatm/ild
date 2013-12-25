ba1d0ee697a8e4f4b6d41dddd3d75fbd69bac8f3
zimbatm <zimbatm@zimbatm.com>
2013-12-25 21:36:35 +0000

    >< Don’t mess with Voodoo

--- build.rb
+++ build.rb
@@ -11,3 +11,7 @@ end
 require 'irb'
 require 'irb/completion'
 IRB.start
+
+# Octokit.create_status 'zimbatm/ild', '75d0d5c999ed6a62d5bda57f1370c92a55c04a93', 'success', target_url: 'https://ild.ngrok.com/foo/bar/baz', description: 'testing <b>the</b> damn thing'
+#
+# Octokit.statuses 'zimbatm/ild', 'master'
